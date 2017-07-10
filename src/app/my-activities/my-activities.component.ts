4ae12a80c01dcb856bfc56ec4bb75354cd28b03f
Jan Vanuytrecht <jan.vanuytrecht@gmail.com>
2017-07-10 21:48:53 +0200

    First try on day vs all view, pbb still needs aria tags and stuff

--- src/app/my-activities/my-activities.component.ts
+++ src/app/my-activities/my-activities.component.ts
@@ -13,8 +13,14 @@ export class MyActivitiesComponent implements OnInit {
 
   data: any;
   dates: any;
+  currentDate: any;
+  showOnlyToday: boolean;
+  currentActivities: any[];
+  currentActivityDateIndex: number;
 
-  constructor(private router: Router, private timetableService: TimeTableService) { }
+  constructor(private router: Router, private timetableService: TimeTableService) { 
+    this.showOnlyToday = true;
+  }
 
   ngOnInit() {
     if (localStorage.getItem('icc_campapp_username') === null) {
@@ -22,23 +28,71 @@ export class MyActivitiesComponent implements OnInit {
     }
 
     this.timetableService.getTimeTable(localStorage.getItem('icc_campapp_username'), localStorage.getItem('icc_campapp_password'))
-    .map((result) => {
-      const timetable = result.timetable.filter(timetableItem => timetableItem.type === 'workshop');
-      result.timetable = timetable;
-      return result;
-    })
-    .subscribe((result) => {
-      this.data = result;
-      const timetable = this.data.timetable;
-      const flags = {}, output = [], length = timetable.length;
+      .map((result) => {
+        const timetable = result.timetable.filter(timetableItem => timetableItem.type === 'workshop');
+        result.timetable = timetable;
+        return result;
+      })
+      .subscribe((result) => {
+        this.data = result;
+        const timetable = this.data.timetable;
+        const flags = {}, output = [], length = timetable.length;
 
-      for (let i = 0; i < length; i++) {
-        if (flags[timetable[i].date]) { continue; }
-        flags[timetable[i].date] = true;
-        output.push(timetable[i].date);
-      }
-      this.dates = output;
-    })
+        for (let i = 0; i < length; i++) {
+          if (flags[timetable[i].date]) { continue; }
+          flags[timetable[i].date] = true;
+          output.push(timetable[i].date);
+        }
+        this.dates = output;
+
+        this.setInitialActivities();
+      })
   }
 
+  setInitialActivities() {
+    if (this.dates.length > 0) {
+      let today = new Date();
+      this.currentActivityDateIndex = 0;
+      this.currentDate = this.dates[0];
+      for (let date of this.dates) {
+        let datePart = date.toString().substring(date.indexOf(',') + 2, date.length);
+        let day = Number(datePart.substring(0, 2));
+        let month = Number(datePart.substring(3, 5));
+        let year = Number(datePart.substring(6, 10));
+        if (year == today.getFullYear() && month == today.getMonth() && day == today.getDate()) {
+          this.currentDate == date;
+          break;
+        }
+      }
+
+      this.currentActivities = this.data.timetable.filter(tte => tte.date == this.currentDate);
+    }
+  }
+
+  selectDayBefore() {
+    // let currentDateIndex = this.dates.indexOf(this.currentDate);
+    if(this.currentActivityDateIndex > 0) {
+      this.currentActivityDateIndex--;
+      this.currentDate = this.dates[this.currentActivityDateIndex];
+      this.currentActivities = this.data.timetable.filter(tte => tte.date == this.currentDate);
+    }
+  }
+
+  selectDayAfter() {
+    // let currentDateIndex = this.dates.indexOf(this.currentDate);
+    if(this.currentActivityDateIndex < this.dates.length - 1) {
+      this.currentActivityDateIndex++;
+      this.currentDate = this.dates[this.currentActivityDateIndex];
+      this.currentActivities = this.data.timetable.filter(tte => tte.date == this.currentDate);
+    }
+  }
+
+  showTodaysActivities() {
+    this.showOnlyToday = true;
+      this.setInitialActivities();
+  }
+
+  showAllActivities() {
+    this.showOnlyToday = false;
+  }
 }
